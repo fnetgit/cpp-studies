4e9fc3bd262493c849450daaaf3dc4981f2c74a2
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-02 00:21:01 -0300

    feat: add comments to clarify data types and their sizes in types.cpp

--- types.cpp
+++ types.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main()
 {
 
-    int number = 1;
-    float float_number = 1.0f;
-    double double_number = 1.0;
+    int number = 1; 
+    float float_number = 1.0f; // 5 bytes/ ~7 digits
+    double double_number = 1.0; // 8 bytes/ ~15-17 digits
     long long_number = 1L;
     long long long_long_number = 1LL;
     bool boolean_value = true;
-    char character = 'A';
+    char character = 'A'; // 1 byte
     string text = "Hello, World!";
 
     return 0;
