a5f5acbdfceb6f0b155039780151f6e83d8cf04c
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-13 21:00:12 -0300

    perf(exercises): add sbc-2025-j-v1.1.cpp

--- exercises/sbc-2025-j-v1.1.cpp
+++ exercises/sbc-2025-j-v1.1.cpp
@@ -8,7 +8,7 @@ int main()
     cin.tie(0);
 
     unordered_set<int> difs;
-    vector<int> d = {1, 2, 3, 4}; //ver depois se com array dá certo
+    int d[] = {1, 2, 3, 4};
     int count = 0;
 
     for (int i = 0; i < 10; i++)
